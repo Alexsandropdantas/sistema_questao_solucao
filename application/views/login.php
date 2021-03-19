3d0e0fafccb1285172070f092184db396f0f2304
Alexsandro Paulino Dantas <alexsandropdantas@gmail.com>
2021-03-19 02:33:47 -0300

    Ajuste tela login

--- application/views/login.php
+++ application/views/login.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-
 	
 <!doctype html>
 <html lang="pt_br">
@@ -11,71 +10,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<meta name="description" content="">
 		<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
 		<meta name="generator" content="Hugo 0.80.0">
-		<title>Signin Template · Bootstrap v5.0</title>
+		<title>Sistema para base de conhecimento dos bots</title>
 		
-		
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+		<!-- Bootstrap 5 css -->
+		<link href="<?php echo base_url('assets/bootstrap-5.0.0-beta2/dist/css/bootstrap.min.css'); ?>" rel="stylesheet">
 
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-	html,
-	body {
-	height: 100%;
-	}
-
-	body {
-	display: flex;
-	align-items: center;
-	padding-top: 40px;
-	padding-bottom: 40px;
-	background-color: #f5f5f5;
-	}
-
-	.form-signin {
-	width: 100%;
-	max-width: 330px;
-	padding: 15px;
-	margin: auto;
-	}
-	.form-signin .checkbox {
-	font-weight: 400;
-	}
-	.form-signin .form-control {
-	position: relative;
-	box-sizing: border-box;
-	height: auto;
-	padding: 10px;
-	font-size: 16px;
-	}
-	.form-signin .form-control:focus {
-	z-index: 2;
-	}
-	.form-signin input[type="email"] {
-	margin-bottom: -1px;
-	border-bottom-right-radius: 0;
-	border-bottom-left-radius: 0;
-	}
-	.form-signin input[type="password"] {
-	margin-bottom: 10px;
-	border-top-left-radius: 0;
-	border-top-right-radius: 0;
-	}
-
-    </style>
+		<!-- CSS customizado tela Login-->
+		<link href="<?php echo base_url('assets/style_customizado_sistema_questao_solucao_login.css'); ?>" rel="stylesheet">
   </head>
   <body class="center">
   		<main class="form-signin">
